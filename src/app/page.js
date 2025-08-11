f8ecc7c501943c82aa2d9aef2b08d7ad4c5aac34
Eric Chen <167908754+Airplane356@users.noreply.github.com>
2025-08-11 11:51:30 -0400

    initial config and setup

--- src/app/page.js
+++ src/app/page.js
@@ -2,102 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <p className="leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in quam ultrices, auctor velit at, ullamcorper mi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas nisi nunc, imperdiet at elit ut, ornare laoreet lectus. Curabitur pretium, dolor nec elementum vehicula, felis nulla eleifend ligula, nec tincidunt leo neque a lacus. Proin condimentum dignissim bibendum. Duis dignissim ex in nisl sodales imperdiet. Vestibulum tempus orci eu condimentum tristique. Duis ultrices nisl risus, ac malesuada justo auctor quis. Nulla consequat, justo a pellentesque pellentesque, erat diam pellentesque mi, in viverra felis est id dui. Cras elit ante, molestie et urna aliquam, tincidunt rutrum nisl. Fusce eget bibendum felis. Fusce id iaculis mi. Nunc id orci suscipit, malesuada sapien id, venenatis leo. Nullam nec vestibulum nulla.
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      Sed scelerisque non velit id tincidunt. Mauris vitae tempor dui. Proin auctor mi id fermentum dapibus. Fusce suscipit, dui at malesuada ornare, sapien tortor iaculis nulla, tempus ultricies metus felis ut nulla. Sed aliquet, dui non convallis vestibulum, dui elit facilisis odio, a condimentum erat velit id neque. Maecenas volutpat pulvinar libero a ultrices. Nam tempus consequat neque eget tincidunt. Morbi mauris orci, vehicula nec efficitur a, mollis id libero. Nulla placerat imperdiet posuere. Morbi nisi sapien, ullamcorper quis ligula a, mattis varius risus. Etiam imperdiet diam nisi, sed hendrerit dui scelerisque eu. Duis arcu nibh, feugiat eget iaculis eget, tincidunt sed erat.
+
+      Suspendisse non aliquet lorem. Pellentesque convallis dui id cursus gravida. Maecenas odio mauris, gravida eu tortor vitae, scelerisque commodo enim. Vivamus tempor suscipit libero eu elementum. Donec eleifend non tellus at ultrices. Nunc mattis vulputate accumsan. Nam rutrum cursus mauris, sed molestie sapien posuere at. Fusce lobortis nisi enim, egestas tristique sem efficitur a. In ut justo vulputate eros molestie volutpat. Aliquam eu lobortis risus, sit amet consectetur enim. Aenean faucibus justo et tempor pellentesque. Etiam aliquet arcu eget aliquet efficitur. Proin porta convallis dapibus. Nam feugiat condimentum dui, eu efficitur mi elementum in. Vivamus porttitor nisi nec dolor rutrum aliquet.
+
+      Pellentesque ultrices imperdiet orci ac euismod. Sed eleifend sem augue, sed cursus augue pellentesque ut. Nulla non lobortis lectus. Sed auctor molestie sapien, eget rhoncus sem molestie id. Integer blandit feugiat ultricies. In vestibulum consequat urna, eget congue justo porta sit amet. Ut a lacinia purus, luctus cursus ante. Mauris dapibus tincidunt convallis.
+
+      Praesent at lectus eleifend, ullamcorper mi a, volutpat ante. In sit amet bibendum enim, eget ullamcorper augue. Aenean elementum, felis nec imperdiet dictum, metus orci lacinia tellus, eu vulputate ex diam quis nisl. Etiam ut rutrum felis. Mauris dictum auctor nulla vitae rhoncus. Aliquam ullamcorper, eros sit amet malesuada elementum, massa leo placerat felis, sed luctus leo metus aliquet enim. Suspendisse potenti. Ut condimentum ut ipsum id condimentum. Integer iaculis dolor eget ligula rutrum accumsan.
+
+      </p>
   );
 }
